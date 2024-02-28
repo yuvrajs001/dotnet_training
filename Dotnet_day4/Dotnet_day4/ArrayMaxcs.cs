f6d2572347c31def27f2de835dad9c9eb8887b17
Yuvraj Singh <yuvrajs@infinite.com>
2024-02-28 12:50:16 +0530

    test day5

--- Dotnet_day4/Dotnet_day4/ArrayMaxcs.cs
+++ Dotnet_day4/Dotnet_day4/ArrayMaxcs.cs
@@ -14,7 +14,6 @@ namespace Dotnet_day4
             Console.Write("Enter the size of the array: ");
             int size = int.Parse(Console.ReadLine());
 
-            int[] arr = new int[size];
 
                 Console.WriteLine("Enter {size} integers:");
                 for (int i = 0; i < size; ++i)
