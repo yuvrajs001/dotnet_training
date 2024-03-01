7dc34ccfdad478a8fafbfeedd30659e8aa93af66
Yuvraj Singh <yuvrajs@infinite.com>
2024-03-01 17:12:54 +0530

    day7

---
+++ CSharp/dotnet_day7/dotnet_day7/exception_eg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_day7
+{
+    class exception_eg
+    { 
+            static void Main(string[] args)
+            {
+                int Number1, Number2, Result;
+                try
+                {
+                    Console.WriteLine("Enter First Number:");
+                    Number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Second Number:");
+                    Number2 = int.Parse(Console.ReadLine());
+                    Result = Number1 / Number2;
+                    Console.WriteLine($"Result = {Result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Message: {ex.Message}");
+                    Console.WriteLine($"Source: {ex.Source}");
+                    Console.WriteLine($"HelpLink: {ex.HelpLink}");
+                    Console.WriteLine($"StackTrace: {ex.StackTrace}");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
---
+++ CSharp/dotnet_day7/dotnet_day7/userdefined_exception.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_day7
+{
+    class VotingException : ApplicationException
+    {
+        public VotingException (String  msg): base (msg)
+        {
+
+        }
+    }
+    class Vote
+    {
+        int age;
+       
+        public void AcceptAge()
+        {
+            Console.WriteLine("enter your age");
+            age = int.Parse(Console.ReadLine());
+            if (age <= 18)
+            {
+                throw (new VotingException("AGE SHOUD BE GREATER THAN"));
+            }
+            else
+            {
+                Console.WriteLine("thanku for voting ");
+            }
+
+         
+        }
+        
+    }
+    class userdefined_exception 
+    {
+        static void Main()
+        {
+            Vote vote = new Vote();
+            try
+            {
+                vote.AcceptAge();
+            }
+            catch(VotingException ve)
+            {
+                Console.WriteLine(ve.Message);
+                
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
